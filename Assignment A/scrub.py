04f6b8c8e7e33e539d32743ce3e86f418979b16f
Christopher Lang <chlang206@gmail.com>
2017-02-22 00:59:27 -0500

    Implemented sliding window file chunker and tested parallelism
    A sliding window generator is implemented. Allows for the developer to
    specify block height and increment size
    
    Also tested basic parallelism on single computer. This is found to be
    significantly slower than serial row cleaning. However, this is expected
    as the row cleaning task is fast, incurring significant process overhead
    
    In next commit, will implement duplicate checking and hopefully normality
    testing to further add computation intensity for each iteration. Hope to
    see this boost CPU utilization to 100%

--- Assignment A/scrub.py
+++ Assignment A/scrub.py
@@ -5,33 +5,65 @@ import logging as lg
 import multiprocessing as mp
 import tqdm
 import ConfigParser
+from joblib import Parallel, delayed
 
-wdir = os.path.dirname(os.path.realpath(__file__))
-os.chdir(wdir)
+# print wdir
+# print "/".join([os.getcwd(), "..", "lib"])
 
-sys.path.append("/".join([os.getcwd(), "..", "lib"]))
 
-import utils
+def row_clean(rows):
+    rows.sort(key=lambda x: x[0])
 
-print wdir
-print "/".join([os.getcwd(), "..", "lib"])
+    for a_row in rows:
+        a_row[1:] = [float(a_row[1]), int(a_row[2])]
 
-# Settings
-configs = ConfigParser.ConfigParser()
-configs.read("config/assignmentA_config.ini")
-chunk_size = configs.getint('readOptions', 'chunkSize')
-block_height = configs.getint('readOptions', 'blockHeight')
-nrows_parsed = 0
+
+# def row_clean2(rows):
+#     return [[relem[0], float(relem[1]), int(relem[2])] for relem in rows]
 
 
 if __name__ == "__main__":
+    wdir = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(wdir)
+
+    sys.path.append("/".join([os.getcwd(), "..", "lib"]))
+
+    import utils
+
+    # Settings
+    configs = ConfigParser.ConfigParser()
+    configs.read("config/assignmentA_config.ini")
+    block_height = configs.getint('chunkOptions', 'blockHeight')
+    window_inc = configs.getint('chunkOptions', 'window_increment')
+    n_cores = configs.getint('computeOptions', 'num_process')
+
+    if n_cores is 0:
+        n_cores = mp.cpu_count()
+
+    else:
+        n_cores = 1 if n_cores < 2 else n_cores
+
+    print("Number of cores: " + str(mp.cpu_count()) + " (" + str(n_cores) + " processes requested)")
+    print("Block height: " + str(block_height))
+    print("Increments: " + str(window_inc))
+
+    nrows_parsed = 0
+
+    # ../Archive/data-big.txt
     with open("../Archive/data-big.txt", "rb") as f:
         reader = csv.reader(f)
 
-        file_chunk = utils.row_chunker(reader, chunk_size=chunk_size)
+        file_chunk = utils.sliding_chunker(reader, chunk_size=block_height,
+                                           inc=window_inc)
 
-        for row in file_chunk:
-            r = [[relem[0], float(relem[1]), int(relem[2])] for relem in row]
-            nrows_parsed += len(r)
+        # Serial iteration
+        # for rows in file_chunk:
+        #     # r = row_clean2(rows)
+        #     row_clean(rows)
+        #     nrows_parsed += len(rows)
 
-    print(nrows_parsed)
+        # Embarrassingly parallel iteration
+        with Parallel(n_jobs=n_cores, verbose=5) as par:
+            par(delayed(row_clean)(row) for row in file_chunk)
+
+    # print(nrows_parsed)
--- lib/utils.py
+++ lib/utils.py
@@ -1,8 +1,26 @@
 import itertools as it
 
 
-def row_chunker(iterator, chunk_size=1000):
-    chunking_iter = iter(iterator)
+def simple_chunker(iterable, chunk_size=1000):
+    """Evenly chunked iterator with generator
+
+    Creates a generator that returns elements within the supplied iterator of
+    approximately equal size. This avoids loading the whole iterator into
+    memory (unless the iterable already exists in memory such as lists)
+
+    Args:
+        iterable (iterable):
+            An object that is iterable, such as iterators, generators, etc.
+
+        chunk_size (int):
+            The size of each returned collection
+
+    Returns (list(iterable)):
+        The generator returns a list of elements retrieved from iterable of
+        length chunk_size or less
+    """
+    chunking_iter = iter(iterable)
+    element_block = list(it.islice(chunking_iter, chunk_size))
 
     while True:
         element_block = list(it.islice(chunking_iter, chunk_size))
@@ -12,3 +30,45 @@ def row_chunker(iterator, chunk_size=1000):
 
         else:
             break
+
+
+def sliding_chunker(iterable, chunk_size=1000, inc=2):
+    """Sliding window chunked iterator with generator
+
+    Creates a generator that returns elements within the supplied iterator of
+    of length chunk_size. Each subsequent call will shift the window forward by
+    inc while maintaining length chunk_size
+
+    Args:
+        iterable (iterable):
+            An object that is iterable, such as iterators, generators, etc.
+
+        chunk_size (int):
+            The size of each returned collection
+
+        inc (int):
+            Sliding window increment size
+
+    Returns (list(iterable)):
+        The generator returns a list of elements retrieved from iterable of
+        length chunk_size or less, shifted by inc forward (except first call)
+    """
+    chunking_iter = iter(iterable)
+    element_block = list(it.islice(chunking_iter, chunk_size))
+
+    if len(element_block) == chunk_size:
+        yield element_block
+
+    counting_index = 0
+    inc_elements = list()
+    for an_element in chunking_iter:
+        inc_elements += [an_element]
+        counting_index += 1
+
+        if (counting_index % inc) is 0:
+            element_block = element_block[inc:] + inc_elements
+
+            yield element_block
+
+            counting_index = 0
+            inc_elements = list()
