659b644be6b5fa94a2db40a74de692ab96276276
Christopher Lang <chlang206@gmail.com>
2017-03-09 23:14:26 -0500

    Committing homework E for professor review

---
+++ Homework E/compute_pi.py
@@ -0,0 +1,77 @@
+from pyspark import SparkContext
+import numpy as np
+
+# We will use the old standard circle and square method
+# The approximation is done using the following formula:
+#  pi ~= 4 * M / N
+# where M is number of points landing in the inscribed circle
+# and N is the number of points generated, in this case iterations
+
+# program setup
+iteration = 100000000
+partition = 4
+radius = 1
+
+
+def pi_estimation(data_packet):
+    n = data_packet['n']
+    radius = data_packet['radius']
+    radius_sq = np.int64(pow(radius, 2))
+    # We utilize numpy arrays for vectorized work
+    # First, center both the square and circle on the origin in cartesian
+    # coordinates
+    # Second, generate n (x, y) points, bounded in the square
+    xpos = np.random.uniform(-1 * radius, radius, n)
+    ypos = np.random.uniform(-1 * radius, radius, n)
+
+    # Third, since the square/circle is centered on origin, we convert the n
+    # (x, y) points into polar coordinates, mainly the radius from origin
+    # If the radii found is beyond the set radius, they're assumed to have
+    # landed in the square. Otherwise, in the inscribed circle
+    #
+    # To get radii, we use pythagoras theorem to find hypotenus
+    in_square = np.sqrt(np.power(xpos, 2) + np.power(ypos, 2)) > radius_sq
+
+    n_square = np.sum(in_square, dtype=np.int64)
+    n_circle = np.int64(in_square.size) - n_square
+
+    result = {
+        "in_circle": n_circle,
+        "in_square": n_square,
+        "nruns": n
+    }
+
+    return result
+
+
+def reduce_estimate(objA, objB):
+    result = {
+        "in_circle": objA['in_circle'] + objB['in_circle'],
+        "in_square": objA['in_square'] + objB['in_square'],
+        "nruns": objA['nruns'] + objB['nruns']
+    }
+
+    return result
+
+
+sc = SparkContext("local", "Pi Circle/Square Approximation")
+
+worker_packet = list()
+for _ in range(0, partition):
+    worker_packet.append({
+        "n": np.int64(iteration / partition),
+        "radius": np.int64(radius)
+    })
+
+distIn = sc.parallelize(worker_packet, partition)
+
+result = distIn.map(pi_estimation).reduce(reduce_estimate)
+
+pi = np.float64(result['in_circle']) / np.float64(result['nruns'])
+pi *= np.float64(4)
+
+nruns = result['nruns']
+
+print("Pi estimation is {0} for {1} simulation points".format(pi, nruns))
+print("N points in circle: {0}".format(result['in_circle']))
+print("N points in square: {0}".format(result['in_square']))
