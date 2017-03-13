9c65b43c16d6d19d4db876b257a6476914204861
Christopher Lang <chlang206@gmail.com>
2017-03-12 22:22:42 -0400

    Implemented parallelized Jarque-Bera normality test in normal.py
    The Jarque-Bera test is based off of Normal Distribution's skewness and
    kurtosis. If the series' skewness and kurtosis is different from a normal
    distribution, the test states that we can reject the null (series is
    normal)
    
    The parallelization comes from the fact that standard deviation, skewness,
    and kurtosis have summable parts. The numerators and denominators are
    computed in parts amongst the workers. The results are sent to the root
    process and the Jarque-Bera test statistic is computed and compared
    
    The wrinkle was the fact that standard deviation, skewness, and kurtosis
    all required the series' sample mean. Therefore, the sample mean is first
    computed amongst the workers (using a similar methodology), aggregated at
    the root process, then sent back to the workers for computing the partial
    standard deviations, skewness, and kurtosis

--- Assignment A/normal.py
+++ Assignment A/normal.py
@@ -5,91 +5,11 @@ import datetime as dt
 from mpi4py import MPI
 import ntpath
 import numpy as np
+from pprint import pprint as pp
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-
-
-def worker(file, row_index, rank, delimiter=",", logger=None,
-           enable_debug=False):
-    start_row = row_index[0]
-    nrows_read = row_index[1] - row_index[0] + 1
-    reader = chk.row_reader(file, start_row, nrows_read)
-
-    if logger is not None and enable_debug:
-            msg = "rank{0} start index: {1}, nrows: {2}"
-            msg = msg.format(rank, start_row, nrows_read)
-            logger.debug(msg)
-
-    nrows_parsed = 0
-    rows = list()
-    for a_row in reader:
-        nrows_parsed += 1
-        rows.append(a_row)
-
-    if "\r\n" in rows[0] and "\r\n" in rows[-1]:
-        rows = [i.split("\r\n")[0] for i in rows]
-
-        if logger is not None and enable_debug:
-                msg = "\\r\\n end of line detected"
-                logger.debug(msg)
-
-    elif "\n" in rows[0] and "\n" in rows[-1]:
-        rows = [i.split("\n")[0] for i in rows]
-
-        if logger is not None and enable_debug:
-                msg = "\\n end of line detected"
-                logger.debug(msg)
-
-    rows = [i.split(delimiter) for i in rows]
-
-    rows = row_clean(rows, start_row, start_row + nrows_read)
-
-    with open(noiseloc, "r") as f:
-        noise_index = f.readlines()
-        noise_index = [int(i) for i in noise_index]
-
-    noise_index = set(noise_index)
-
-    nrows = len(rows)
-    rows = remove_noise(rows, noise_index)
-
-    nrows_denoise = len(rows)
-
-    result = {
-        "is_normal": is_prices_normal(rows),
-        "nrows_org": nrows,
-        "nrows_denoised": nrows_denoise
-    }
-
-    return result
-
-
-def row_clean(rows, start_index, end_index):
-    for a_row, row_index, in zip(rows, range(start_index, end_index)):
-        a_row[1:3] = [float(a_row[1]), int(a_row[2])]
-        a_row += [row_index]
-
-    rows.sort(key=lambda x: x[0])
-
-    result = [tuple(i) for i in rows]
-
-    return result
-
-
-def remove_noise(rows, noise_indices):
-    rows = [i for i in rows if i[-1] not in noise_indices]
-
-    return rows
-
-
-def is_prices_normal(rows):
-    prices = [i[1] for i in rows]
-
-    result = mutils.jarque_bera(prices)
-
-    return result
 
 
 if __name__ == "__main__":
@@ -107,15 +27,14 @@ if __name__ == "__main__":
 
     # Load program settings
     with open("config/assignmentA_config.json", "r") as f:
-        configs = json.load(f)
+        cfg = json.load(f)
 
-    nchunk = configs['chunk_size']
-    n_cores = configs['num_process']
-    row_delim = configs['col_delimiter']
-    enable_debug = configs['enable_debug']
-    noiseloc = configs['noisefileloc']
-    exec_logloc = configs['exec_logloc'].format("normal")
-    result_logloc = configs['result_logloc'].format("normal")
+    nchunk = cfg['chunk_size']
+    n_cores = cfg['num_process']
+    row_delim = cfg['col_delimiter']
+    enable_debug = cfg['enable_debug']
+    noiseloc = cfg['noisefileloc']
+    delimiter = cfg['col_delimiter']
 
     # Create execution logger
     # ---------------------------------------------------------------------
@@ -126,7 +45,7 @@ if __name__ == "__main__":
         log_level = logging.INFO
 
     lg = logging.getLogger()
-    lgr_handler = logging.FileHandler(exec_logloc)
+    lgr_handler = logging.FileHandler(cfg['exec_logloc'].format("normal"))
     lgr_fmt = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
     lgr_handler.setFormatter(lgr_fmt)
     lg.addHandler(lgr_handler)
@@ -144,7 +63,7 @@ if __name__ == "__main__":
 
     if dataloc is None:
         try:
-            dataloc = configs['dataloc']
+            dataloc = cfg['dataloc']
 
         except KeyError:
             # no dataloc was provided in configuration file
@@ -159,13 +78,13 @@ if __name__ == "__main__":
 
         # Create time tracker -------------------------------------------------
         # ---------------------------------------------------------------------
-        import TimeTrack
-        tt = TimeTrack.TimeTrack()
+        import Timetrack
+        tt = Timetrack.Timetrack()
 
         # Create result logger
         # ---------------------------------------------------------------------
-        result_log = rl.ResultLogger("result/noise_testresult.txt",
-                                     configs['prog_title'])
+        result_log = rl.ResultLogger(cfg['result_logloc'].format("normal"),
+                                     cfg['prog_title'])
 
         nrows = chk.get_nrows(dataloc)
 
@@ -186,30 +105,29 @@ if __name__ == "__main__":
             lg.info("MPI size {0}, working in parallel".format(size))
 
         s_index = 0
+        nchunk = cfg['chunk_size']
         nrows_left = nrows
         nworkers = size
-
-        if nworkers < 1:
-            nworkers = 1
-
         scrub_results = list()
-
         worker_row_indices = list()
+        row_indices = list()
         for i in range(nworkers):
             worker_row_indices.insert(i, [])
 
         while nrows_left != 0:
-            row_indices = utils.size_sequencer(nchunk, nworkers, s_index)
-
-            for a_row_indices in range(len(row_indices)):
-                index_interval = row_indices[a_row_indices]
-                worker_row_indices[a_row_indices].append(index_interval)
+            row_indices.append(utils.size_sequencer(nchunk, nworkers, s_index))
 
             nrows_left -= nchunk
             s_index += nchunk
 
             if nchunk > nrows_left:
                 nchunk = nrows_left
+
+        row_indices = [item for sublist in row_indices for item in sublist]
+        row_indices = utils.even_split(row_indices, size)
+
+        for ipair, wp in zip(row_indices, worker_row_indices):
+            wp.extend(ipair)
 
     else:
         worker_row_indices = None
@@ -220,58 +138,160 @@ if __name__ == "__main__":
     row_indices = comm.scatter(worker_row_indices, root=0)
 
     if rank == 0:
-        extt = TimeTrack.TimeTrack()
+        extt = Timetrack.Timetrack()
 
-    work_result = list()
+    # Create row reader object
+    rowreader = chk.Rowread(dataloc, row_indices[0][0])
+
+    # Read in noise indices from scrub.py
+    with open(noiseloc, "r") as f:
+        noise_index = f.readlines()
+        noise_index = set(int(i) for i in noise_index)
+
+    price_mean = dict()
+    price_mean['nrows'] = 0
     for index_interval in row_indices:
-        r = worker(dataloc, index_interval, rank, row_delim, lg, enable_debug)
-        work_result.append(r)
+        rowreader.set_startrow(index_interval[0] + 1)
+        rows = rowreader.read(index_interval[1] - index_interval[0] + 1)
 
-    scrub_results = comm.gather(work_result, root=0)
+        indexed_rows = [a_row for a_row in enumerate(rows, index_interval[0] + 1)]
+        indexed_rows = [i for i in indexed_rows if i[0] not in noise_index]
+
+        prices = (i[1].split(delimiter)[1] for i in indexed_rows)
+        prices = np.fromiter(prices, dtype=np.float64)
+
+        price_mean["total_price"] = np.sum(prices)
+        price_mean["nprices"] = np.int64(prices.size)
+        price_mean['nrows'] += np.int64(len(rows))
+
+    partial_means = comm.gather(price_mean, root=0)
+
+    if rank == 0:
+        price_mean = utils.gather_dict(partial_means)
+
+        price_mean = price_mean['total_price'] / price_mean['nprices']
+
+        print(price_mean)
+
+        packets = [{"mean": price_mean, "i": i} for i in worker_row_indices]
+
+    else:
+        packets = None
+
+    packet = comm.scatter(packets, root=0)
+
+    rowreader.reset(row_indices[0][0] + 1)
+    test_stats = list()
+    for index_interval in packet['i']:
+        rowreader.set_startrow(index_interval[0] + 1)
+        rows = rowreader.read(index_interval[1] - index_interval[0] + 1)
+
+        indexed_rows = [a_row for a_row in enumerate(rows, index_interval[0] + 1)]
+        indexed_rows = [i for i in indexed_rows if i[0] not in noise_index]
+
+        prices = (i[1].split(delimiter)[1] for i in indexed_rows)
+        prices = np.fromiter(prices, dtype=np.float64)
+
+        deviation = prices - packet['mean']
+        pstats = dict()
+        pstats['n'] = 0
+        pstats['stdev_num'] = np.sum(np.power(deviation, 2))
+        pstats['skew_num'] = np.sum(np.power(deviation, 3))
+        pstats['kurt_num'] = np.sum(np.power(deviation, 4))
+        pstats['n'] += np.int64(prices.size)
+
+        test_stats.append(pstats)
+
+        msg = "rank-{0} Start row: {1}, End row: {2}"
+        msg = msg.format(rank, index_interval[0] + 1, index_interval[1] + 1)
+        lg.debug(msg)
+
+        msg = "rank-{0} nprices: {1}".format(rank, prices.size)
+        lg.debug(msg)
+
+        msg = "rank-{0} First price: {1}, dev: {2}".format(rank, prices[0],
+                                                           deviation[0])
+        lg.debug(msg)
+
+        msg = "rank-{0} Last price: {1}, dev: {2}".format(rank, prices[-1],
+                                                          deviation[-1])
+        lg.debug(msg)
+
+    test_stats = utils.gather_dict(test_stats)
+
+    msg = "rank-{0} stdev numerator: {1}".format(rank, test_stats['stdev_num'])
+    lg.info(msg)
+
+    msg = "rank-{0} skew numerator: {1}".format(rank, test_stats['skew_num'])
+    lg.info(msg)
+
+    msg = "rank-{0} kurt numerator: {1}".format(rank, test_stats['kurt_num'])
+    lg.info(msg)
+
+    partial_stats = comm.gather(test_stats, root=0)
 
     if rank == 0:
         extt.pause_time()
+
+        jarque_bera_stats = {
+            "stdev_num": 0, "skew_num": 0, "kurt_num": 0, "n": 0
+        }
+        print("Results from workers:")
+        pp(partial_stats)
+        for partial_stat in partial_stats:
+            for key in jarque_bera_stats:
+                jarque_bera_stats[key] += partial_stat[key]
+
+        print("Aggregated:")
+        pp(jarque_bera_stats)
+
+        final_stdev = jarque_bera_stats['stdev_num']
+        final_stdev /= (jarque_bera_stats['n'] - 1)
+        final_stdev = np.sqrt(final_stdev)
+
+        final_skew = jarque_bera_stats['skew_num'] / jarque_bera_stats['n']
+        final_skew /= np.power(final_stdev, 3)
+
+        final_kurt = jarque_bera_stats['kurt_num'] / jarque_bera_stats['n']
+        final_kurt /= np.power(final_stdev, 4)
+
+        jb_chisq = jarque_bera_stats['n'] / np.float64(6)
+        jb_chisq *= np.power(final_skew, 2) + (np.power(final_kurt - 3, 2))
+        jb_chisq /= np.float64(4)
+
         tt.pause_time()
 
-        scrub_results = [item for sublist in scrub_results for item in sublist]
+        lg.info("stdev: {0}, skew: {1}, kurt: {2}".format(final_stdev,
+                                                          final_skew,
+                                                          final_kurt))
+        lg.info("Jarque-Bera test statistic: {0}".format(jb_chisq))
+        lg.info("Null hypothesis: Series is likely normally distributed")
+        lg.info("significance 95% (JB Stat <= 5.99): {0}".format(jb_chisq <= np.float64(5.99)))
+        lg.info("significance 99% (JB Stat <= 9.21): {0}".format(jb_chisq <= np.float64(9.21)))
 
-        r = dict()
-        r['n_normal_true'] = []
-        r['all_true'] = False
-        r['nrows_parsed'] = 0
-        r['nrows_denoised'] = 0
+        analysis_output = list()
+        analysis_output.append(("Execution start time",
+                                tt.start_time_pretty()))
+        analysis_output.append(("Execution end time", tt.end_time_pretty()))
+        analysis_output.append(("Execution elapsed time", tt.elapsed_pretty()))
+        analysis_output.append(("Row parse elapsed time",
+                                extt.elapsed_pretty()))
+        # result_log.init_section("Analysis Output", level=0)
 
-        for a_result in scrub_results:
-            r['n_normal_true'].append(a_result['is_normal'])
-            r['nrows_parsed'] += a_result['nrows_org']
-            r['nrows_denoised'] += a_result['nrows_denoised']
+        velocity = jarque_bera_stats['n'] * 2 / extt.elapsed_seconds()
+        velocity = str(round(velocity, 2))
+        analysis_output.append(("Velocity (rows parsed / sec)", velocity))
 
-        r['all_true'] = all(r['n_normal_true'])
-        r['n_normal_true'] = [1 for i in r['n_normal_true'] if i is True]
-        n_not_true = np.sum([1 for i in r['n_normal_true'] if i is False])
-        r['n_normal_true'] = round(np.sum(r['n_normal_true']), 1)
+        analysis_output.append(("# of prices", str(jarque_bera_stats['n'])))
+        analysis_output.append(("Mean (price)", str(round(price_mean, 4))))
+        analysis_output.append(("STDEV (price)", str(round(final_stdev, 4))))
+        analysis_output.append(("Skewness (price)", str(round(final_skew, 4))))
+        analysis_output.append(("Kurtosis (price)", str(round(final_kurt, 4))))
+        analysis_output.append(("Jarque-Bera test statistic", str(round(jb_chisq, 4))))
+        analysis_output.append(("Null hypothesis", "Price is normal"))
+        analysis_output.append(("Compared to", "Chi-square, 2 dof"))
+        analysis_output.append(("significance 95% (JB Stat <= 5.99)", str(jb_chisq <= np.float64(5.99))))
+        analysis_output.append(("significance 99% (JB Stat <= 9.21)", str(jb_chisq <= np.float64(9.21))))
 
-        result_log.init_section("Analysis Output", level=0)
-        result_log.add_section_kv("Execution start time",
-                                  tt.start_time_pretty())
-        result_log.add_section_kv("Execution end time", tt.end_time_pretty())
-        result_log.add_section_kv("Execution elapsed time",
-                                  tt.elapsed_pretty())
-        result_log.add_section_kv("Row parse elapsed time",
-                                  extt.elapsed_pretty())
-        result_log.add_section_kv("Row count", r['nrows_parsed'])
-        result_log.add_section_kv("Row count (denoised)", r['nrows_denoised'])
-        result_log.add_section_kv("All normal", r['all_true'])
-        result_log.add_section_kv("# chunks not normal", n_not_true)
+        result_log.add_lines(result_log.section("Analysis Output", result_log.kv_format(analysis_output)))
 
-        velocity = r['nrows_parsed'] / extt.elapsed_seconds()
-        velocity = round(velocity, 2)
-        result_log.add_section_kv("Velocity (rows parsed / sec)", velocity)
-        # result_log.add_section_kv("Noise rows (negative num.)",
-        #                           r['n_negativeNum'])
-        # result_log.add_section_kv("Noise rows (# of columns)",
-        #                           r['n_wrongLength'])
-        # result_log.add_section_kv("Noise rows (timestamp format)",
-        #                           r['n_timestampFormat'])
-
-        result_log.exec_section()
